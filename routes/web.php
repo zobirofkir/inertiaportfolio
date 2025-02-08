68f23fe0cc9a1a6e52b619ba01d7a7734c2c14e9
zobirofkir <zobirofkir19@gmail.com>
2025-02-08 22:22:13 +0100

    setup envirement

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return inertia('Home');
 });
+
+require __DIR__.'/auth.php';

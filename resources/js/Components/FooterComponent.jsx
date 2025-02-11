d72facb6caa85cb08782d8b1394648eb7f1d4528
zobirofkir <zobirofkir19@gmail.com>
2025-02-11 23:38:11 +0100

    update

--- resources/js/Components/FooterComponent.jsx
+++ resources/js/Components/FooterComponent.jsx
@@ -7,7 +7,7 @@ const FooterComponent = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="text-center md:text-left mb-4 md:mb-0">
             <p className="text-sm">
-              &copy; {new Date().getFullYear()} Your Name. All rights reserved.
+              &copy; {new Date().getFullYear()} Zobir Ofkir. All rights reserved.
             </p>
           </div>
           <div className="flex space-x-4">

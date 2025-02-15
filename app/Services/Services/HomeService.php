a745c836f3f24188e68a9294afd671dbcf833652
zobirofkir <zobirofkir19@gmail.com>
2025-02-15 22:45:32 +0100

    create limit for categories and for tags

--- app/Services/Services/HomeService.php
+++ app/Services/Services/HomeService.php
@@ -11,8 +11,8 @@ class HomeService implements HomeConstructor
 {
     public function index()
     {
-        $categories = Category::all();
-        $tags = Tag::pluck('title');
+        $categories = Category::latest()->take(20)->get();
+        $tags = Tag::pluck('title')->take(20);
         $blogs = Blog::latest()->with('category', 'tags')->take(16)->get();
         return inertia('Home', ['categories' => $categories, 'tags' => $tags, 'blogs' => $blogs]);
     }

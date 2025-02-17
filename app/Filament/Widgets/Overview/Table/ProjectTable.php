8b73797dee7a4df76b2cc7b44da9f6187b39e8f8
zobirofkir <zobirofkir19@gmail.com>
2025-02-17 15:05:46 +0100

    create project column

--- app/Filament/Widgets/Overview/Table/ProjectTable.php
+++ app/Filament/Widgets/Overview/Table/ProjectTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Widgets\Overview;
 
 use App\Models\Project;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -19,10 +20,10 @@ class ProjectTable extends BaseWidget
                 Project::query()
             )
             ->columns([
+                ImageColumn::make('image'),
                 TextColumn::make('title'),
                 TextColumn::make('slug'),
                 TextColumn::make('description'),
-                TextColumn::make('image'),
                 TextColumn::make('status'),
                 TextColumn::make('created_at'),
                 TextColumn::make('updated_at'),

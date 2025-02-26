3111351ef707e8f933e6d51caf6eab1f338517ed
zobirofkir <zobirofkir19@gmail.com>
2025-02-26 19:13:53 +0000

    traduire arabic to english

--- resources/js/Components/SliderComponent.jsx
+++ resources/js/Components/SliderComponent.jsx
@@ -64,7 +64,9 @@ const SliderComponent = ({projectsSlider}) => {
                   }}
                 ></div>
 
-                {/* محتوى البطاقة */}
+                {/**
+                 * Cart Content
+                 */}
                 <motion.div
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.95 }}

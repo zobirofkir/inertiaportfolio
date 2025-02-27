f1631c80d17b772cc6a1573b2608315e59226029
zobirofkir <zobirofkir19@gmail.com>
2025-02-27 14:10:10 +0000

    change style

--- resources/js/Components/SkillComponent.jsx
+++ resources/js/Components/SkillComponent.jsx
@@ -7,15 +7,6 @@ const SkillComponent = ({ skills }) => {
   const [play] = useSound(clickSound);
   const [isBookOpen, setIsBookOpen] = useState(false);
 
-  // Define array of colors
-  const bgColors = [
-    'bg-gradient-to-r from-blue-500 via-blue-400 to-blue-300',
-    'bg-gradient-to-r from-green-500 via-green-400 to-green-300',
-    'bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-300',
-    'bg-gradient-to-r from-red-500 via-red-400 to-red-400',
-    'bg-gradient-to-r from-purple-500 via-purple-400 to-purple-300',
-  ];
-
   const handleFlip = () => {
     play();
     setIsBookOpen(true);
@@ -53,30 +44,29 @@ const SkillComponent = ({ skills }) => {
             <h1 className="text-4xl font-bold mb-4 z-10">📚 ZOBIR OFKIR</h1>
             <p className="text-lg z-10">Hello I'm Zobir Ofkir And This Is My Skills !</p>
             {/* Text on the right side */}
-            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 text-sm text-gray-400">
+            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 text-sm text-white">
               <p>Flip to explore my skills</p>
             </div>
             {/* Text on the left side */}
-            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 text-sm text-gray-400">
+            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 text-sm text-white">
               <p>Click to Open</p>
             </div>
           </div>
 
           {/* Skill Pages */}
           {skills.map((skill, index) => {
-            const randomColor = bgColors[Math.floor(Math.random() * bgColors.length)];
 
             return (
               <div
                 key={skill.id}
-                className={`p-6 rounded-lg shadow-lg flex flex-col items-center text-center ${randomColor} relative`}
+                className={`p-6 rounded-lg shadow-lg flex flex-col items-center text-center dark:bg-transparent bg-gray-300  relative`}
               >
                 {/* Decorative text on the left side */}
-                <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 md:text-xl text-md text-white">
+                <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 md:text-xl text-md text-black dark:text-white">
                   <p>Page {index + 1}</p>
                 </div>
                 {/* Decorative text on the right side */}
-                <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 md:text-xl text-md text-white">
+                <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 md:text-xl text-md text-black dark:text-white">
                   <p>Zobir Ofkir</p>
                 </div>
 
@@ -86,26 +76,26 @@ const SkillComponent = ({ skills }) => {
                   className="w-16 h-16 mb-4"
                 />
 
-                <h2 className="md:text-3xl text-xl font-bold text-white">
+                <h2 className="md:text-3xl text-xl font-bold text-black dark:text-white">
                   {skill.title} {skill.emoji}
                 </h2>
 
-                <p className="text-gray-600 mt-2 md:text-2xl text-md text-white font-bold">{skill.description}</p>
+                <p className="text-gray-600 mt-2 md:text-2xl text-md text-black dark:text-white font-bold">{skill.description}</p>
               </div>
             );
           })}
 
           {/* Back Cover */}
-          <div className="cover bg-gray-800 rounded-xl text-white flex flex-col justify-center items-center p-6 relative overflow-hidden">
+          <div className="cover bg-gray-800 rounded-xl text-black dark:text-white flex flex-col justify-center items-center p-6 relative overflow-hidden">
             <div className="absolute inset-0 bg-cover bg-center opacity-20" style={{ backgroundImage: 'url(https://www.transparenttextures.com/patterns/dark-leather.png)' }}></div>
-            <h1 className="text-4xl font-bold mb-4 z-10">📚 ZOBIR OFKIR</h1>
-            <p className="text-lg z-10">Thanks for exploring my skills!</p>
+            <h1 className="text-4xl font-bold text-white mb-4 z-10">📚 ZOBIR OFKIR</h1>
+            <p className="text-lg z-10 text-white">Thanks for exploring my skills!</p>
             {/* Text on the left side */}
-            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 text-sm text-gray-400">
+            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -rotate-90 text-sm text-white">
               <p>Hope you enjoyed the journey</p>
             </div>
             {/* Text on the right side */}
-            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 text-sm text-gray-400">
+            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 rotate-90 text-sm text-white">
               <p>Let's build something amazing!</p>
             </div>
           </div>

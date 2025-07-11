63822ce037fdf34c5ec92c348c1570a422a5c7a9
csw <zobirofkir19@gmail.com>
2025-07-11 23:05:37 +0100

    remove automatique get widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,6 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 /**
                  * Overview Widget

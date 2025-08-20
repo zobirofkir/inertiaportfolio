a133e27dc90cbdc2d66d8fdb383225b61dd1d7ad
csw <zobirofkir19@gmail.com>
2025-08-20 22:39:31 +0100

    create blog request

---
+++ app/Http/Requests/BlogRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BlogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "user_id" => "required|exists:users,id",
+            "category_id" => "required|exists:categories,id",
+            "title" => "required|string|max:255",
+            "description" => "required|string",
+            "content" => "required|string",
+            "images" => "nullable|array",
+            "is_published" => "required|boolean",
+        ];
+    }
+}

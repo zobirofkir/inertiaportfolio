8d3e3bb4fdf61573f8a05b49a53c4500f6be2a77
csw <zobirofkir19@gmail.com>
2025-08-20 22:34:36 +0100

    create login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::prefix("auth")->group(function() {
+    Route::post('/login', [LoginController::class, 'login']);
+});

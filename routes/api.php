f274db53cc7d6eb19732044805e75ea8b0db98da
csw <zobirofkir19@gmail.com>
2025-08-20 22:43:52 +0100

    create

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\BlogApiController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix("auth")->group(function() {
     Route::post('/login', [LoginController::class, 'login']);
+
+    Route::middleware(['auth:api'])->group(function() {
+        Route::apiResource("blogs", BlogApiController::class);
+    });
 });
+
